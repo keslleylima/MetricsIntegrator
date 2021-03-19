59abc26fa176b84b96b2a1aaca247b614d3f0118
William Niemiec <williamniemiec@hotmail.com>
2021-03-18 22:44:26 -0300

    Doc

--- src/MetricsIntegrator.IO/MetricsFileManager.cs
+++ src/MetricsIntegrator.IO/MetricsFileManager.cs
@@ -1,8 +1,13 @@
 ﻿using MetricsIntegrator.Utils;
+using System;
+using System.IO;
 using System.Text.RegularExpressions;
 
 namespace MetricsIntegrator.IO
 {
+    /// <summary>
+    ///     Responsible for finding metrics files.
+    /// </summary>
     public class MetricsFileManager
     {
         //---------------------------------------------------------------------
@@ -29,9 +34,43 @@ namespace MetricsIntegrator.IO
         //---------------------------------------------------------------------
         //		Methods
         //---------------------------------------------------------------------
-        public void FindAllFromDirectory(string basePath)
+        /// <summary>
+        ///     Find metrics files, from a given directory path, with the 
+        ///     following prefixes:
+        ///     <list type="bullet">
+        ///         <item>
+        ///             <term>'SC_'</term>
+        ///             <description>Source code metrics path</description>
+        ///         </item>
+        ///         <item>
+        ///             <term>'MAP_'</term>
+        ///             <description>Mapping path</description>
+        ///         </item>
+        ///         <item>
+        ///             <term>'TC_'</term>
+        ///             <description>Test case metrics path</description>
+        ///         </item>
+        ///         <item>
+        ///             <term>'TP_'</term>
+        ///             <description>Test paths metrics path</description>
+        ///         </item>
+        ///     </list>
+        /// </summary>
+        /// 
+        /// <param name="dirPath">Directory path</param>
+        /// 
+        /// <exception cref="System.ArgumentException">
+        ///     If directory path is null or invalid
+        /// </exception>
+        public void FindAllFromDirectory(string dirPath)
         {
-            string[] csvFilesPath = FileUtils.GetAllFilesFromDirectoryEndingWith(basePath, "csv");
+            if (dirPath == null)
+                throw new ArgumentException("Directory path cannot be null");
+
+            if (!Directory.Exists(dirPath))
+                throw new ArgumentException("Invalid directory path");
+
+            string[] csvFilesPath = FileUtils.GetAllFilesFromDirectoryEndingWith(dirPath, "csv");
             
             foreach (string csvPath in csvFilesPath)
             {
@@ -49,8 +88,43 @@ namespace MetricsIntegrator.IO
             }
         }
 
+        /// <summary>
+        ///     Defines metrics files from cli following the following 
+        ///     structure:
+        ///     <list type="bullet">
+        ///         <item>
+        ///             <term>-sc</term>
+        ///             <description>Source code metrics file flag</description>
+        ///         </item>
+        ///         <item>
+        ///             <term>-map</term>
+        ///             <description>Mapping file flag</description>
+        ///         </item>
+        ///         <item>
+        ///             <term>-tc</term>
+        ///             <description>Test case metrics file flag</description>
+        ///         </item>
+        ///         <item>
+        ///             <term>-tp</term>
+        ///             <description>Test paths metrics file flag</description>
+        ///         </item>
+        ///     </list>
+        /// </summary>
+        /// 
+        /// <example>
+        ///     <code>-sc sourcecode.csv -map map.csv -tc testcase.csv -tp testpath.csv</code>
+        /// </example>
+        /// 
+        /// <param name="args">CLI arguments</param>
+        /// 
+        /// <exception cref="System.ArgumentException">
+        ///     If args is null or empty.
+        /// </exception>
         public void SetFilesFromCLI(string[] args)
         {
+            if ((args == null) || args.Length == 0)
+                throw new ArgumentException("Arguments cannot be empty");
+
             int idxParamStart = IsFlag(args[0]) ? 0 : 1;
 
             for (int i = idxParamStart; i < args.Length; i++)
