1eee48bff671d79670dd0abf0a14e4d1f583e7ea
William Niemiec <williamniemiec@hotmail.com>
2021-03-19 17:48:57 -0300

    Tests - refactoring

--- test/MetricsIntegrator.Parser/MappingMetricsParserTest.cs
+++ test/MetricsIntegrator.Parser/MappingMetricsParserTest.cs
@@ -1,29 +1,89 @@
 ﻿using MetricsIntegratorTest;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using Xunit;
 
 namespace MetricsIntegrator.Parser
 {
     public class MappingMetricsParserTest
     {
+        //---------------------------------------------------------------------
+        //		Attributes
+        //---------------------------------------------------------------------
+        private readonly string basePath;
+        private string filename;
+        private string testedInvoked;
+        private Dictionary<string, List<string>> obtained;
+        private Dictionary<string, List<string>> expected;
+
+
+        //---------------------------------------------------------------------
+        //		Constructor
+        //---------------------------------------------------------------------
+        public MappingMetricsParserTest()
+        {
+            basePath = GenerateBasePath();
+            expected = new Dictionary<string, List<string>>();
+        }
+
+
+        //---------------------------------------------------------------------
+        //		Tests
+        //---------------------------------------------------------------------
         [Fact]
         public void TestParse()
         {
-            string basePath = PathManager.GetResourcesPath() + Path.DirectorySeparatorChar 
-                + "MetricsIntegrator.Parser" + Path.DirectorySeparatorChar + "map-test.csv";
-            
-            MappingMetricsParser parser = new MappingMetricsParser(basePath);
-            Dictionary<string, List<string>> obtained = parser.Parse();
-            Dictionary<string, List<string>> expected = new Dictionary<string, List<string>>()
-            {
-                { "pkgname1.pkgname2.ClassName1.testedMethod1()", new List<string>() { "pkgname3.ClassName2.testMethod1()", "pkgname3.ClassName2.testMethod2()" } },
-                { "pkgname1.pkgname2.ClassName1.testedMethod2(SomeClass<T>, SomeClass2...)", new List<string>() { "pkgname3.ClassName2.testMethod1()" } },
-                { "pkgname1.pkgname2.ClassName1.testedMethod3(SomeClass2...)", new List<string>() { "pkgname3.ClassName2.testMethod1()" } }
-            };
+            UsingFile("map-test.csv");
+            WithTestedInvoked("pkgname1.pkgname2.ClassName1.testedMethod1()");
+            BindTestMethods("pkgname3.ClassName2.testMethod1()", "pkgname3.ClassName2.testMethod2()");
 
+            WithTestedInvoked("pkgname1.pkgname2.ClassName1.testedMethod2(SomeClass<T>, SomeClass2...)");
+            BindTestMethods("pkgname3.ClassName2.testMethod1()");
+
+            WithTestedInvoked("pkgname1.pkgname2.ClassName1.testedMethod3(SomeClass2...)");
+            BindTestMethods("pkgname3.ClassName2.testMethod1()");
+
+            DoParsing();
+
+            AssertParseIsCorrect();
+        }
+
+
+        //---------------------------------------------------------------------
+        //		Methods
+        //---------------------------------------------------------------------
+        private string GenerateBasePath()
+        {
+            return  PathManager.GetResourcesPath()
+                    + Path.DirectorySeparatorChar
+                    + "MetricsIntegrator.Parser"
+                    + Path.DirectorySeparatorChar;
+        }
+
+        private void UsingFile(string filename)
+        {
+            this.filename = filename;
+        }
+
+        private void WithTestedInvoked(string signature)
+        {
+            this.testedInvoked = signature;
+        }
+
+        private void BindTestMethods(params string[] testMethods)
+        {
+            expected.Add(testedInvoked, new List<string>(testMethods));
+        }
+
+        private void DoParsing()
+        {
+            MappingMetricsParser parser = new MappingMetricsParser(basePath + filename);
+
+            obtained = parser.Parse();
+        }
+
+        private void AssertParseIsCorrect()
+        {
             Assert.Equal(expected, obtained);
         }
     }
