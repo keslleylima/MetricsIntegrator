c176bcc97eb3d11c4618e93024b78f804175407a
William Niemiec <williamniemiec@hotmail.com>
2021-03-20 19:02:07 -0300

    Doc

--- src/MetricsIntegrator.Parser/MetricsParseManager.cs
+++ src/MetricsIntegrator.Parser/MetricsParseManager.cs
@@ -6,6 +6,16 @@ using System.Text;
 
 namespace MetricsIntegrator.Parser
 {
+    /// <summary>
+    ///     Responsible for parsing metrics files of the following files:
+    ///     
+    ///     <list type="bullet">
+    ///         <item>Source code metrics</item>
+    ///         <item>Mapping of tested methods along with the test method that tests it</item>
+    ///         <item>Test path metrics</item>
+    ///         <item>Test case metrics</item>
+    ///     </list>
+    /// </summary>
     public class MetricsParseManager
     {
         //---------------------------------------------------------------------
@@ -18,12 +28,38 @@ namespace MetricsIntegrator.Parser
         //---------------------------------------------------------------------
         //		Constructor
         //---------------------------------------------------------------------
+        /// <summary>
+        ///     Parses files contained in a metric file manager.
+        /// </summary>
+        /// 
+        /// <param name="metricsFileManager">Metric files</param>
+        /// <param name="delimiter">Symbol used to separate data</param>
+        /// 
+        /// <exception cref="System.ArgumentException">
+        ///     If metrics file manager is null or if delimiter is null or empty.
+        /// </exception>
         public MetricsParseManager(MetricsFileManager metricsFileManager, string delimiter)
         {
+            if (metricsFileManager == null)
+                throw new ArgumentException("Metrics file manager cannot be null");
+
+            if ((delimiter == null) || delimiter.Length == 0)
+                throw new ArgumentException("Delimiter cannot be null");
+
             this.metricsFileManager = metricsFileManager;
             this.delimiter = delimiter;
         }
 
+        /// <summary>
+        ///     Parses files contained in a metric file manager. Using this
+        ///     constructor, delimiter will be ';'.
+        /// </summary>
+        /// 
+        /// <param name="metricsFileManager">Metric files</param>
+        /// 
+        /// <exception cref="System.ArgumentException">
+        ///     If metrics file manager is null or if delimiter is null or empty.
+        /// </exception>
         public MetricsParseManager(MetricsFileManager metricsFileManager) 
             : this(metricsFileManager, ";")
         {
